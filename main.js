b3c6d87a4282af5d34f39a7033d4e5ec32fa6507
ikarus-pritish <pritish.sehzpaul@ikarus3d.com>
2023-02-21 17:39:14 +0530

    changes for deployment #2

--- main.js
+++ main.js
@@ -60,12 +60,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('./space2.jpg');
+const spaceTexture = new THREE.TextureLoader().load('./public/space2.jpg');
 scene.background = spaceTexture;
 
 // Avatar
 
-const peronTexture = new THREE.TextureLoader().load('./_dsc2747.jpg');
+const peronTexture = new THREE.TextureLoader().load('./public/_dsc2747.jpg');
 
 const person = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: peronTexture }));
 
@@ -73,8 +73,8 @@ scene.add(person);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('./moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('./normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('./public/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('./public/normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
